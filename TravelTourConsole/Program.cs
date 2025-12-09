72d20af2f3adf4fa2a69ffaf0aad6fdd893395b0
2926 <2926@PC0975>
2025-12-09 17:02:13 +0330

    add sample

--- TravelTourConsole/Program.cs
+++ TravelTourConsole/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using TravelTourConsole.Services;
+
+Start();
+
+static void Start()
+{
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("Menu");
+        Console.WriteLine("1.Register");
+        Console.WriteLine("2.Tour");
+        Console.WriteLine("3.Reservation");
+        Console.WriteLine("3.Exit");
+
+        Console.Write("Please Choice: ");
+        string choice = Console.ReadLine();
+
+        if (choice == "4")
+            return;
+
+        if (choice == "1")
+        {
+            var registrationService = new RegistrationService();
+            registrationService.Run();
+        }
+        else if (choice == "2")
+        {
+
+        }
+
+    }
+}
---
+++ TravelTourConsole/Services/RegistrationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelTourConsole.Services
+{
+    public class RegistrationService
+    {
+        public void Run()
+        {
+            Console.Clear();
+            Console.WriteLine("******* SubMenu *******");
+            Console.WriteLine("11.Registeration");
+        }
+    }
+}
